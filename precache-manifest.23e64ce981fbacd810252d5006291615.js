7681df3a8eadd129f7cd673ca8cd63c8cee1ffcc
freshkoala <zunigaruizb@gmail.com>
2020-04-23 15:32:14 +0800

    Updates

---
+++ precache-manifest.23e64ce981fbacd810252d5006291615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "625ef6953fb0715103fed904a00c07d1",
+    "url": "/guessing-game/static/media/font.625ef695.otf"
+  },
+  {
+    "revision": "baffc29f3af61ede29d9",
+    "url": "/guessing-game/static/js/runtime~main.baffc29f.js"
+  },
+  {
+    "revision": "5cf242b3bec757488238",
+    "url": "/guessing-game/static/js/main.5cf242b3.chunk.js"
+  },
+  {
+    "revision": "18db35bb5687d5a42fde",
+    "url": "/guessing-game/static/js/1.18db35bb.chunk.js"
+  },
+  {
+    "revision": "5cf242b3bec757488238",
+    "url": "/guessing-game/static/css/main.9aae4f72.chunk.css"
+  },
+  {
+    "revision": "02ea08fb3605ea9283c0c2a3c88fcc25",
+    "url": "/guessing-game/index.html"
+  }
+];
